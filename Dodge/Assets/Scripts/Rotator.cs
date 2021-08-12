854aad42a79edf1c804a45580acbc71f6681ef58
kmc94 <money94611@naver.com>
2021-08-12 21:42:44 +0900

    레트로 제1게임 닷지 최종버전

---
+++ Dodge/Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSPeed = 60f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, rotationSPeed * Time.deltaTime, 0f);
+    }
+}
